3c0d55f26a3a74e92d54bfae56f652e6f248bbae
Maneesha Kaushalya <148955760+maneesha2002@users.noreply.github.com>
2025-02-02 10:08:42 +0530

    delete

---
+++ Frontend/app.js
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from './src/screens/LoginScreen'; // Updated path
+import RegisterScreen from './src/screens/RegisterScreen'; // Updated path
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="Register" component={RegisterScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
